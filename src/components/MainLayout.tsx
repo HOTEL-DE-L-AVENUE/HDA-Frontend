d75ca7f79ed507ab9fee537dae40d3b32952a17a
Theophile <rasolofosontheophile43@gmail.com>
2026-09-24 11:01:15 +0300

    okok

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -5,15 +5,17 @@ import { Header } from "./Header";
 export default function MainLayout() {
   return (
     <div
-      className="min-h-screen w-full overflow-x-hidden"
+      className="flex h-dvh w-full overflow-hidden"
       style={{ fontFamily: "Inter, sans-serif" }}
     >
       <Sidebar />
 
-      <div className="md:ml-[72px] w-full md:w-[calc(100%-72px)] min-h-screen">
-        <Header />
+      <div className="flex min-h-0 w-full flex-1 flex-col md:ml-[72px] md:w-[calc(100%-72px)]">
+        <div className="sticky top-0 z-40 flex-shrink-0">
+          <Header />
+        </div>
 
-        <main className="p-4 pb-28 md:p-6 md:pb-6 lg:p-8">
+        <main className="min-h-0 flex-1 overflow-y-auto overflow-x-hidden p-4 pb-28 md:p-6 md:pb-6 lg:p-8">
           <Outlet />
         </main>
       </div>
